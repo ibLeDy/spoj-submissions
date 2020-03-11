71a4cc403723e884661dd48c47f080de8033a5f0
bLeDy <deejaynof@gmail.com>
2020-03-11 18:14:03 +0100

    Improve solution (1.55s-1.04s)

--- INTEST/main.py
+++ INTEST/main.py
@@ -1,6 +1,4 @@
 from sys import stdin, stdout
 
 n, k = [int(i) for i in stdin.readline().split()]
-nums = [int(stdin.readline()) for _ in range(n)]
-divisible = [True if num % k == 0 else False for num in nums]
-stdout.write(str(divisible.count(True)))
+stdout.write(str(sum(1 for num in stdin.read().split() if int(num) % k == 0)))
